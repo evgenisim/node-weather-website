4f61859157470fbf446d83af8815fe1a0224458e
shopifydev-es <shopifydev@gmail.com>
2019-04-03 11:09:05 +0300

    Initial commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,22 @@
+const request = require('request');
+
+const forecast = (lat, lng, callback) => {
+  const url = `https://api.darksky.net/forecast/4182996366b36284abff094283e580bb/${lat},${lng}?units=si`;
+
+  request({
+    url,
+    json: true
+  }, (erorr, {
+    body
+  }) => {
+    if (erorr) {
+      callback('Unable to connect to weather service!', undefined);
+    } else if (body.error) {
+      callback('Unable to find location', undefined);
+    } else {
+      callback(undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability}% chance of rain.`)
+    }
+  })
+}
+
+module.exports = forecast;
