5a5c55d2645422eb9abca373b1f3e6942a995cc3
shopifydev-es <shopifydev@gmail.com>
2019-04-04 09:52:06 +0300

    Add more forecast data

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,5 +1,7 @@
 const request = require('request');
 
+
+
 const forecast = (lat, lng, callback) => {
   const url = `https://api.darksky.net/forecast/4182996366b36284abff094283e580bb/${lat},${lng}?units=si`;
 
@@ -14,7 +16,7 @@ const forecast = (lat, lng, callback) => {
     } else if (body.error) {
       callback('Unable to find location', undefined);
     } else {
-      callback(undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability}% chance of rain.`)
+      callback(undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability}% chance of rain. Humidity is ${body.daily.data[0].humidity} and the wind speed is ${body.daily.data[0].windSpeed}. The maximum temperature is ${body.daily.data[0].temperatureHigh} and the minimum is ${body.daily.data[0].temperatureLow}`)
     }
   })
 }
